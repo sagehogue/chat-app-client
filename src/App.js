e4ecf87ff91804007f58325a68bee76af53ba1bc
Will <will.lucas1996@gmail.com>
2020-05-26 14:57:11 -0700

    homepage in progress

--- src/App.js
+++ src/App.js
@@ -4,13 +4,14 @@ import Chat from "./components/Chat/Chat";
 import Join from "./components/Join/Join";
 import LoginPage from "./components/LoginPage/LoginPage";
 import GlobalStyle from "./components/GlobalStyles/GlobalStyles";
-
+import HomePage from "./components/HomePage/HomePage";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 
 const App = () => {
   return (
     <Router>
-      <Route path="/" exact component={LoginPage} />
+      <Route path="/" exact component={HomePage} />
+      <Route path="/login" component={LoginPage} />
       <Route path="/chat" component={Chat} />
 
       <GlobalStyle />
