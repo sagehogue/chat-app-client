2528a1b8b30d39088804e93eca05a52503f51670
Will <will.lucas1996@gmail.com>
2020-05-25 19:47:01 -0700

    temp loggout button

--- src/components/LoginPage/LoginPage.js
+++ src/components/LoginPage/LoginPage.js
@@ -6,7 +6,6 @@ import * as firebase from "firebase/app";
 import "firebase/auth";
 import "firebase/firestore";
 
-
 import styled from "styled-components";
 import Login from "../Login/Login";
 import Register from "../Register/Register";
@@ -105,6 +104,34 @@ const SignInButton = styled.input`
   }
 `;
 
+const LogOutButton = styled.input`
+  color: #fff !important;
+  text-transform: uppercase;
+  text-decoration: none;
+  background: rgba(41, 121, 255, 0.75);
+  padding: 1.25rem;
+  border-radius: 5px;
+  display: inline-block;
+  border: none;
+  width: 100%;
+  margin-top: 1.5rem;
+  transition: all 0.15s;
+
+  &:hover {
+    transform: scale(1.1) translateY(-0.5rem);
+    box-shadow: 0rem 0.15rem #333;
+    background: rgba(41, 121, 255, 1);
+  }
+  & :focus {
+    outline: none;
+    border: none;
+  }
+  & :active {
+    outline: none;
+    border: none;
+  }
+`;
+
 export default function LoginPage() {
   const [name, setName] = useState("");
   const [room, setRoom] = useState("");
@@ -129,11 +156,11 @@ export default function LoginPage() {
     // Initialize Firebase
     firebase.initializeApp(firebaseConfig);
     // Event listener for auth status.
-    firebase.auth().onAuthStateChanged(function (user) {
+    firebase.auth().onAuthStateChanged(function(user) {
       if (user) {
         // User is signed in.
-        console.log(user)
-        setAuthenticated(true)
+        console.log(user);
+        setAuthenticated(true);
         // example data
         // var displayName = user.displayName;
         // var email = user.email;
@@ -145,80 +172,93 @@ export default function LoginPage() {
         // ...
       } else {
         // User is signed out.
-        setAuthenticated(false)
+        setAuthenticated(false);
         // ...
       }
     });
     return () => {
       // cleanup
-    }
-  }, [])
-
-
+    };
+  }, []);
 
   const handleDisplayRegisterForm = () => {
     // changes the variable that controls which form is displayed
     setShowRegisterForm(true);
-    setHeading("Registration")
+    setHeading("Registration");
   };
 
-  const handleLoginSubmit = (e) => {
-    e.preventDefault()
+  const handleLoginSubmit = e => {
+    e.preventDefault();
     const form = e.target;
-    const email = form.email.value
-    const password = form.password.value
+    const email = form.email.value;
+    const password = form.password.value;
     console.log(`
     Login Submit Successful!\n
     email: ${email}
     \n password: ${password}
-    `)
-    firebase.auth().signInWithEmailAndPassword(email, password).catch(function (error) {
-      // Handle Errors here.
-      var errorCode = error.code;
-      var errorMessage = error.message;
-      // ...
-    });
-  }
-
-  const handleRegisterSubmit = (e) => {
-    e.preventDefault()
-    const form = e.target;
-    const email = form.email.value
-    const username = form.username.value
-    const password = form.password.value
-    console.log(`Form submit successful!\n username: ${username}\npassword: ${password}\nemail: ${email}`)
-    firebase.auth().createUserWithEmailAndPassword(email, password).then(res => {
-      const user = firebase.auth().currentUser;
-
-      // This is how you update properties on the profile.
-      user.updateProfile({
-        displayName: username,
-      }).then(function () {
-        // Update successful.
-        // Code to prepare the room join screen goes here.
-      }).catch(function (error) {
-        return console.log('Error! Account failed to update. Error: ' + error)
+    `);
+    firebase
+      .auth()
+      .signInWithEmailAndPassword(email, password)
+      .catch(function(error) {
+        // Handle Errors here.
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        // ...
       });
-      setHeading("Chatter")
-    }).catch(function (error) {
-      // Handle Errors here.
-      var errorCode = error.code;
-      var errorMessage = error.message;
-      alert(
-        `ERROR ${errorCode}\n
+  };
+
+  const handleRegisterSubmit = e => {
+    e.preventDefault();
+    const form = e.target;
+    const email = form.email.value;
+    const username = form.username.value;
+    const password = form.password.value;
+    console.log(
+      `Form submit successful!\n username: ${username}\npassword: ${password}\nemail: ${email}`
+    );
+    firebase
+      .auth()
+      .createUserWithEmailAndPassword(email, password)
+      .then(res => {
+        const user = firebase.auth().currentUser;
+
+        // This is how you update properties on the profile.
+        user
+          .updateProfile({
+            displayName: username
+          })
+          .then(function() {
+            // Update successful.
+            // Code to prepare the room join screen goes here.
+          })
+          .catch(function(error) {
+            return console.log(
+              "Error! Account failed to update. Error: " + error
+            );
+          });
+        setHeading("Chatter");
+      })
+      .catch(function(error) {
+        // Handle Errors here.
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        alert(
+          `ERROR ${errorCode}\n
         ${error.message}`
-      )
-      // ...
-    });
+        );
+        // ...
+      });
     // Create new account
-  }
-
-
+  };
 
   return (
     <OuterFormContainer>
       <InnerFormContainer>
-        <LoginForm showRegisterForm={showRegisterForm} onSubmit={handleLoginSubmit}>
+        <LoginForm
+          showRegisterForm={showRegisterForm}
+          onSubmit={handleLoginSubmit}
+        >
           <Heading>{`${heading}`}</Heading>
           <EmailInput
             required
@@ -242,9 +282,14 @@ export default function LoginPage() {
             </Link>  This is from the room link component*/}
             <SignInButton type="submit" value="Sign In" />
             <Register clickHandler={() => handleDisplayRegisterForm()} />
+            <LogOutButton type="submit" value="Logout" />
           </AuthButtons>
         </LoginForm>
-        <RegisterForm heading={heading} showRegisterForm={showRegisterForm} handleSubmit={handleRegisterSubmit} />
+        <RegisterForm
+          heading={heading}
+          showRegisterForm={showRegisterForm}
+          handleSubmit={handleRegisterSubmit}
+        />
       </InnerFormContainer>
     </OuterFormContainer>
   );
