04157cde056ad1052fdbc3bbf6f0b9ac2e780385
Sage Hogue <37313170+sagehogue@users.noreply.github.com>
2020-06-07 21:39:32 -0700

    began work on roomstab

--- src/pages/HomePage/RoomsTab/RoomsTab.js
+++ src/pages/HomePage/RoomsTab/RoomsTab.js
@@ -1,10 +1,15 @@
+import React from 'react'
 import styled from 'styled-components';
+
+import { FaSearch } from 'react-icons/fa'
 import Theme from '../../../util/Theme/Theme'
+import Toolbox from './Toolbox/Toolbox'
 
-
-const RoomsTab = styled.section`
+const RoomsTabStyle = styled.section`
   grid-row: 1 / -1;
   grid-column: 3 / 4;
+  display: flex;
+  flex-direction: row-reverse;
   width: 100%;
   height: 100%;
   background: rgba(255, 255, 255, 0.3);
@@ -15,4 +20,30 @@ const RoomsTab = styled.section`
   z-index: 2;
 `;
 
-export default RoomsTab
+const RoomsList = styled.div`
+display: flex;
+flex-direction: column;
+height: 100vw;
+background-color: red;
+`
+
+const Rooms = styled.div`
+
+`
+
+const FavoriteRooms = styled.div`
+
+`
+
+export default function RoomsTab({ pageOnDisplay }) {
+  return (
+    <RoomsTabStyle pageOnDisplay={pageOnDisplay}>
+      <RoomsList>
+        <FaSearch />
+        <FavoriteRooms />
+        <Rooms />
+      </RoomsList>
+      <Toolbox />
+    </RoomsTabStyle>
+  )
+}
