aefdf1f49f8504c62964197f4bd3f33779ac58bb
Sage Hogue <37313170+sagehogue@users.noreply.github.com>
2020-06-30 14:03:54 -0700

    fixed some issues with the message css

--- src/components/Messages/Message/Message.js
+++ src/components/Messages/Message/Message.js
@@ -7,7 +7,7 @@ import { FaRegSmileBeam } from 'react-icons/fa'
 // TODOS:
 
 // figure out how to lookup user's profile pic on firebase
-
+// Move profile to the side of the message instead of above it.
 
 
 
@@ -18,33 +18,40 @@ position: relative;
   margin-top: .3rem;
   width: 100%;
   & > :nth-child(1) {
-      margin: ${ props => props.sentByUser ? "0 auto auto 0" : "auto"};
-      min-width: ${ props => props.sentByUser ? "0" : "50vw"};
+      /* margin: ${ props => props.sentByUser ? "0 auto auto 0" : "auto"}; */
+      /* min-width: ${ props => props.sentByUser ? "0" : "50vw"}; */
   }
   & > :nth-child(2) {
-    margin: ${ props => props.sentByUser ? "auto" : "0 0 auto auto"};
-    min-width: ${ props => props.sentByUser ? "50vw" : "0"};
+    /* margin: ${ props => props.showProfile ? "0 0 0 1.5rem" : "0"}; */
+    /* min-width: ${ props => props.sentByUser ? "50vw" : "0"}; */
   }
+  
 `
 
 const TextBubble = styled.p`
+text-align: center;
 background-color: ${props => props.sentByUser ? "#2979ff" : "#f3f3f3"};
-padding: .5rem;
+padding: .5rem .25rem;
 border-radius: 1rem;
-margin-top: ${props => props.showProfile ? "1rem" : "0"};
-margin-bottom: ${props => props.showProfile ? "1.5rem" : "0"};
-transform: translateY(${props => props.showProfile ? "1.5rem" : "0"})
+margin: ${props => props.showProfile ? "3.3rem auto 0 auto" : "0 auto"};
+width: 95%;
+/* margin-top: ; */
+/* transform: translateX(${props => props.showProfile ? "2.5vw" : "0"}); */
+@media screen and (min-width: 900px) {
+    padding:  1rem .5rem;
+  }
 `
 
 const Profile = styled.div`
 text-align: center;
 position: absolute;
-top: 0;
+top: -.1rem;
 left: 0;
 display: ${props => props.showProfile ? "flex" : "none"};
 flex-direction: column;`
 
-const ProfilePic = styled.div``;
+const ProfilePic = styled.div`
+font-size: 1.5rem;`;
 const Username = styled.span``;
 
 export default function Message({ message, user, shouldDisplayUsername = false }) {
--- src/components/Messages/Messages.js
+++ src/components/Messages/Messages.js
@@ -1,6 +1,5 @@
-import React, { useState } from "react";
+import React, { useEffect, useRef } from "react";
 
-import ScrollToBottom from "react-scroll-to-bottom";
 
 import styled from "styled-components";
 
@@ -9,34 +8,41 @@ import Message from "./Message/Message";
 // TODO: 
 // Smarter message sorting
 // Save the right messages, display SYSTEM messages differently
-// Currently 
+// Get ScrollToBottom working
+// Make a prettier/clearer way to display messages from SYSTEM/admin
 
 const MessagesStyle = styled.div`
-  padding: 5% 0;
+padding: 1.5% 0 1% 0;
   flex: auto;
+  min-height: 70vh;
+  overflow: hidden;
+
 `;
 const Messages = ({ messages, name }) => {
+  const messagesEndRef = useRef(null)
+  const scrollToBottom = () => {
+    messagesEndRef.current.scrollIntoView({ behavior: "smooth" })
+  }
+  useEffect(scrollToBottom, [messages]);
   console.log(messages)
   let lastUserToSendMessage;
   // if (messages.length) {
   //   lastUserToSendMessage = messages[messages.length - 1].user
   // }
   return (
-    < MessagesStyle >
-      <ScrollToBottom>
-        {messages.map((message, i) => {
-          let differentUser = lastUserToSendMessage !== message.user ? true : false
-          lastUserToSendMessage = message.user
-          return (
-            <div key={i}>
-              <Message
-                message={message}
-                user={name}
-                shouldDisplayUsername={differentUser || lastUserToSendMessage === undefined ? true : false} />
-            </div>
-          )
-        })}
-      </ScrollToBottom>
+    <MessagesStyle>
+      {messages.map((message, i) => {
+        let differentUser = lastUserToSendMessage !== message.user ? true : false
+        lastUserToSendMessage = message.user
+        return (
+          <Message
+            key={i}
+            message={message}
+            user={name}
+            shouldDisplayUsername={(differentUser || lastUserToSendMessage === undefined) && message.user !== "admin" ? true : false} />
+        )
+      })}
+      <div ref={messagesEndRef} />
     </MessagesStyle >
   )
 };
